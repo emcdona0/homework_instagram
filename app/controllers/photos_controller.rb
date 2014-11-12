fa153b75561b948d29937f57b7d0d41939fdc614
emcdona0 <emcdonald@chicagobooth.edu>
2014-11-11 20:26:14 -0600

    almost done

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -3,5 +3,70 @@ class PhotosController < ApplicationController
     @list_of_photos = Photo.all
   end
 
+def show
+
+photo_num = params[:id]
+
+@photo = Photo.find_by({:id => photo_num})
+
 
 end
+
+
+
+def new_form
+end
+
+def create_row
+
+@photo = Photo.new
+@photo.source = params[:the_source]
+@photo.caption = params[:the_caption]
+@photo.save
+end
+
+
+  def edit_form
+  photo_num = params[:id]
+  @photo = Photo.find_by({:id => photo_num})
+  # pass these two variable to the view
+  #@photo.source  @photo.caption
+
+  end
+
+  def update_row
+  @photo = Photo.find_by(params[:id])
+  @photo.source = params[:the_source]
+  @photo.caption = params[:the_caption]
+  @photo.save
+  end
+
+
+def destroy
+photo_num = params[:id]
+@photo = Photo.find_by({:id => photo_num})
+@photo.destroy
+
+end
+
+
+
+end
+
+
+
+#Routes
+#Define the action in the controller
+#Make sure that controller is temp_controller
+#Make sure the views folder and the viewes file have the same name as the controller
+
+
+  # def sign = params[:the_sign]
+
+  #   z = Zodiac.find_by({ :sign => params [:the_sign]})
+
+  #   @fortune = z.fortune
+  # end
+
+
+# i = Instructor.find_by({ :first_name => "Raghu" })
